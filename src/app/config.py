7cb4c3df2368a1a352c9479e2a8da358bcd31cb6
luanarchaves <luanarodrigueschaves50@gmail.com>
2026-05-27 18:48:10 -0300

    feat: add document processing and storage functionality with embeddings

--- src/app/config.py
+++ src/app/config.py
@@ -1,5 +1,9 @@
 # S3
 import  boto3
+import os
+from huggingface_hub import InferenceClient
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 s3 = boto3.client(
     "s3",
@@ -10,3 +14,12 @@ s3 = boto3.client(
 )
 
 BUCKET_NAME = "meu-bucket"
+
+# Configurações de Caminho e Token
+CHROMA_PATH = "./chroma_db"
+
+# Inicialização dos Embeddings (Modelo leve para CPU)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Inicialização do Banco Vetorial
+db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embeddings)
--- src/app/doc_services.py
+++ src/app/doc_services.py
@@ -4,6 +4,12 @@ from docling.document_converter import DocumentConverter
 from pathlib import Path
 import io
 import boto3
+import os
+import shutil
+from docling.document_converter import DocumentConverter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.config import db
+
 
 """
 Serviços de coleta, formatação e armazenamento dos documentos
@@ -12,17 +18,10 @@ Serviços de coleta, formatação e armazenamento dos documentos
     banco vetorial
 """
 
-
-s3 = boto3.client(
-    "s3",
-    endpoint_url="http://localhost:4566",  
-    aws_access_key_id="test",
-    aws_secret_access_key="test",
-    region_name="us-east-1"
-)
-
-
 async def read_file(file: UploadFile):
+    """
+    Coleta arquivo e verifica tipo e tamanho
+    """
     
     allowTypeDocument = ["application/pdf", "text/plain"]
     
@@ -41,6 +40,10 @@ async def read_file(file: UploadFile):
         
     
 def docling_convert(file):
+    """
+    Converte documento em Marckdown
+    """
+        
     try:
         converter = DocumentConverter()
         filename = (str(file.filename)).replace(" ", "_").lower()
@@ -49,22 +52,44 @@ def docling_convert(file):
         conteudo = doc.export_to_markdown()
         file_md = io.BytesIO(conteudo.encode("utf-8"))
         
+
         
         return file_md, filename
     except Exception as e:
         print(e)
         return None, None
         
-        
-def printa(file_md: io.BytesIO):
+def convert_data_and_store(file_md, filename):
+    """
+    Converte documento em chunk, depois embadding e armazena no DB 
+    """
+
     file_md.seek(0)
-    print(file_md.read().decode("utf-8"))
+    chunks = text_splitter.split_text(file_md)
+
+    #EMBADDING
+
+    db.add_texts(texts=chunks, metadatas=[{"source": filename}] * len(chunks))
+
 
 
 def search_localstack(file_md: io.BytesIO, filename):
+    """
+    Procura documento no localstack
+    """
 
     return{
         "message": "Erro ao salvar arquivo no localstack.",
     }
+
+def set_data_localstack(file_md: io.BytesIO, filename):
+    """
+    Atualiza metadados do documento no localstack
+    """
+
+    return{
+        "message": "Erro ao salvar arquivo no localstack.",
+    }
+
 
 
